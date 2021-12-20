b857c6909262a0ec2496c8736e37afc9fe7bcb58
ravira1 <sowmyr@cox.net>
2021-12-20 10:08:33 -0700

    Second commit

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ terraform {
  }
  }
 }
-provider "aws" {
- assume_role {
-  role_arn = "arn:aws:iam::140882719977:role/polyadmin"
- }
- access_key = var.access_key
- secret_key = var.secret_key
- region = var.region
-}
+#provider "aws" {
+ #assume_role {
+  #role_arn = "arn:aws:iam::140882719977:role/polyadmin"
+ #}
+ #access_key = var.access_key
+ #secret_key = var.secret_key
+ #region = var.region
+#}
 resource "aws_instance" "snow_discovery" {
   ami = "ami-0b7d0db7325d1918a"
   instance_type = "t2.micro"
